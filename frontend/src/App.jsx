0c11169d0f71f3d49d013afd22bc48c63f064c45
DialloJones333 <diallojones23@gmail.com>
2024-05-22 18:26:30 -0400

    Simple frontend configuration to update favicon and page title to EcoEfficient Living.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,13 +10,13 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={energyLogo} className="logo" alt="My logo" />
+          <img src={energyLogo} className="logo" alt="Energy logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>EcoEfficient Living</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      Click on the Vite and React logos to learn more
       </p>
     </>
   )
