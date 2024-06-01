5146c6e3e17097e97e7427d3b013ceef844613c1
DialloJones333 <diallojones23@gmail.com>
2024-06-01 09:15:00 -0400

    Update package dependencies and add prettier-plugin-tailwindcss

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -8,17 +8,22 @@ const Dashboard = () => {
             <Navbar />
             <div className="flex flex-row gap-10 mt-10">
                 <div className="flex flex-col items-start ms-4 gap-10">
-                    <div className="w-96 h-64 bg-gray-200 flex items-center justify-center rounded-lg shadow-md">
+                    <div className="w-104 h-72 bg-gray-200 flex items-center justify-center rounded-lg shadow-md">
                         
                     </div>
-                    <div className="w-96 h-64 bg-gray-200 flex items-center justify-center rounded-lg shadow-md">
+                    <div className="w-104 h-72 bg-gray-200 flex items-center justify-center rounded-lg shadow-md">
                         
                     </div>
-                    <div className="w-96 h-64 bg-gray-200 flex items-center justify-center rounded-lg shadow-md">
+                    <div className="w-104 h-72 bg-gray-200 flex items-center justify-center rounded-lg shadow-md">
                         
                     </div>
                 </div>
                 <div className="flex flex-col items-center gap-10 w-full">
+                    <div>
+                        <h1 className="text-6xl font-bold font-serif">
+                            Dashboard
+                        </h1>
+                    </div>
                     <div className="w-full max-w-4xl h-96 bg-gray-200 shadow-lg flex items-center justify-center rounded-lg">
                         
                     </div>
