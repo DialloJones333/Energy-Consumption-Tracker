257f6d5bf26933e5f5c34c7062266a8564a0cb9e
DialloJones333 <diallojones23@gmail.com>
2024-06-02 13:41:45 -0400

    Update Dashboard page layout and styles to display device management button

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -7,9 +7,16 @@ const Dashboard = () => {
         <div className="dashboard-page bg-gray-100 min-h-screen p-5">
             <Navbar />
             <div className="flex flex-row gap-10 mt-10">
-                <div className="flex flex-col items-start ms-4 gap-10">
-                    <div className="w-104 h-72 bg-gray-200 flex items-center justify-center rounded-lg shadow-md">
+                <div className="flex flex-col ms-4 gap-10">
+                    <div className="w-104 h-72 bg-gray-200 flex items-center justify-center rounded-lg shadow-lg">
                         
+                    </div>
+                    <div className="flex flex-row justify-center">
+                        <a href="/">
+                            <button className="btn btn-lg btn-outline shadow-md border-slate-800 hover:bg-green-500 text-slate-800 font-bold">
+                                Manage Devices
+                            </button>
+                        </a>
                     </div>
                     <div className="w-104 h-72 bg-gray-200 flex items-center justify-center rounded-lg shadow-md">
                         
