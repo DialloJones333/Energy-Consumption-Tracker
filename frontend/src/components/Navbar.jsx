e0cf8ce3372eb361ff25f53f749556adaff3d833
DialloJones333 <diallojones23@gmail.com>
2024-06-03 09:00:29 -0400

    Update Navbar component with new dropdown menu items

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,8 +9,9 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </button>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow-md bg-gray-100 rounded-box w-52">
-                            <li><a href="/">Homepage</a></li>
-                            <li><a href="/">Portfolio</a></li>
+                            <li><a href="/device-management">Device Management</a></li>
+                            <li><a href="/">Comparison Tools</a></li>
+                            <li><a href="/">Tips & Tricks</a></li>
                             <li><a href="/">About</a></li>
                         </ul>
                     </div>
@@ -35,9 +36,10 @@ const Navbar = () => {
                         <li>
                             <a href="/" className="justify-between">
                                 Profile <span className="badge">New</span>
-                            </a> 
+                            </a>
                         </li>
-                        <li><a href="/">Settings</a></li>
+                        <li><a href="/">Account Preferences</a></li>
+                        <li><a href="/">Notifications</a></li>
                         <li><a href="/">Logout</a></li>
                     </ul>
                 </div>
