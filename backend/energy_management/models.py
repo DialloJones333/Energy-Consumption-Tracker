7d55d28a8c59e1b4322e0c4b34bb047a64ff0c65
DialloJones333 <diallojones23@gmail.com>
2024-06-07 17:07:02 -0400

    refactor: Remove address field from UserProfile model

--- backend/energy_management/models.py
+++ backend/energy_management/models.py
@@ -6,7 +6,6 @@ from django.core.validators import RegexValidator
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=255, blank=True)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(max_length=15, blank=True)
 
