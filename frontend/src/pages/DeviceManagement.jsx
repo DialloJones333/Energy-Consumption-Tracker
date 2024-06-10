806458932bc56aad9ae4a700685a0b01410a45d5
DialloJones333 <diallojones23@gmail.com>
2024-06-10 10:41:54 -0400

    refactor: Update DeviceManagement component layout and styles for consistency

--- frontend/src/pages/DeviceManagement.jsx
+++ frontend/src/pages/DeviceManagement.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 
 const DeviceManagement = () => {
     return (
-        <div className="flex flex-col min-h-screen" >
+        <div className="flex flex-col min-h-screen p-5" >
             <Navbar />
             <div className="flex flex-grow items-center justify-center p-10">
                 <DeviceManager />
