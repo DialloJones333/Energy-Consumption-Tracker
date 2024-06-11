4c5094477b40c6ac36c402315263152afc94ba40
DialloJones333 <diallojones23@gmail.com>
2024-06-10 20:47:01 -0400

    update settings imports in respective files

--- backend/energy_configs/asgi.py
+++ backend/energy_configs/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Energy_Consumption_Tracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energy_configs.settings.development')
 
 application = get_asgi_application()
--- backend/energy_configs/wsgi.py
+++ backend/energy_configs/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Energy_Consumption_Tracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energy_configs.settings.development')
 
 application = get_wsgi_application()
