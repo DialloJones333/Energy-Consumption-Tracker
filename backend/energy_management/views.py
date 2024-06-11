7a3b594f78f8e78a5e4ca710a1f74706bc09cf93
DialloJones333 <diallojones23@gmail.com>
2024-06-10 21:12:13 -0400

    Update views.py to use viewsets and serializers for database models

--- backend/energy_management/views.py
+++ backend/energy_management/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from .models import UserProfile, Device, ConsumptionRecord, Tip, Notification
+from .serializers import UserSerializer, UserProfileSerializer, DeviceSerializer, ConsumptionRecordSerializer, TipSerializer, NotificationSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+class DeviceViewSet(viewsets.ModelViewSet):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+class ConsumptionRecordViewSet(viewsets.ModelViewSet):
+    queryset = ConsumptionRecord.objects.all()
+    serializer_class = ConsumptionRecordSerializer
+
+class TipViewSet(viewsets.ModelViewSet):
+    queryset = Tip.objects.all()
+    serializer_class = TipSerializer
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
