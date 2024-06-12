e05b6f37f49f50d6e4b6bd921e9b16c666241a13
DialloJones333 <diallojones23@gmail.com>
2024-06-12 12:39:29 -0400

    chore: Update routing in App.jsx for authentication and private routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,40 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PrivateRoute from '../services/PrivateRoute';
 import LandingPage from './pages/LandingPage';
-import SignUp from './pages/Signup';
+import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import DeviceManagement from './pages/DeviceManagement';
 import CompareRates from './pages/CompareRates';
 import TipsAndTricks from './pages/TipsAndTricks';
 import Profile from './pages/Profile';
-import ChangePassword from './pages/ChangePassword'
+import ChangePassword from './pages/ChangePassword';
 import AccountPreferences from './pages/AccountPreferences';
 import Notifications from './pages/Notifications';
-
+import { AuthProvider } from '../services/AuthContext';
 
 function App() {
-
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/device-management' element={<DeviceManagement />} />
-        <Route path='/compare-rates' element={<CompareRates />} />
-        <Route path='/tips-and-tricks' element={<TipsAndTricks />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/change-password' element={<ChangePassword />} />
-        <Route path='/account-preferences' element={<AccountPreferences />} />
-        <Route path='/notifications' element={<Notifications />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/device-management" element={<DeviceManagement />} />
+            <Route path="/compare-rates" element={<CompareRates />} />
+            <Route path="/tips-and-tricks" element={<TipsAndTricks />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/change-password" element={<ChangePassword />} />
+            <Route path="/account-preferences" element={<AccountPreferences />} />
+            <Route path="/notifications" element={<Notifications />} />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
-export default App
+export default App;
