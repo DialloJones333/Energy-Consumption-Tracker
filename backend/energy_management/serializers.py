5aa52d848a61932df26ba64883cb63d7f0d5210d
DialloJones333 <diallojones23@gmail.com>
2024-06-14 08:08:26 -0400

    chore: Remove duplicate imports

--- backend/energy_management/serializers.py
+++ backend/energy_management/serializers.py
@@ -5,12 +5,6 @@ from django.contrib.auth.password_validation import validate_password
 from .models import UserProfile, Device, ConsumptionRecord, Tip, Notification
 
 
-# Serializer for handling user registration
-from rest_framework import serializers
-from django.contrib.auth.models import User
-from django.contrib.auth.password_validation import validate_password
-
-
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True, required=True, validators=[validate_password]
