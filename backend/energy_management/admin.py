ee8c8517de4548d586d4ea443b1962e460e9008b
DialloJones333 <diallojones23@gmail.com>
2024-06-21 07:37:23 -0400

    feat: Register MonthlyConsumption model in admin.py

--- backend/energy_management/admin.py
+++ backend/energy_management/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Device, ConsumptionRecord, Tip, Notification, UserProfile
+from .models import Device, ConsumptionRecord, MonthlyConsumption, Tip, Notification, UserProfile
 
 admin.site.register(Device)
 admin.site.register(ConsumptionRecord)
+admin.site.register(MonthlyConsumption)
 admin.site.register(Tip)
 admin.site.register(Notification)
 admin.site.register(UserProfile)
