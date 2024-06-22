d9abe395162296dc4b42ead7ee843bd28e2f675f
DialloJones333 <diallojones23@gmail.com>
2024-06-21 22:26:49 -0400

    feat: Begin integrating the monthly data into my YearlyChart component to display historical monthly data on the users dashboard

--- frontend/src/components/Charts/YearlyChart.jsx
+++ frontend/src/components/Charts/YearlyChart.jsx
@@ -5,62 +5,50 @@ const data = [
     {
         name: 'Jan',
         Usage: 4500,
-        Goal: 5000,
     },
     {
         name: 'Feb',
         Usage: 3300,
-        Goal: 4298,
     },
     {
         name: 'Mar',
         Usage: 5200,
-        Goal: 6800,
     },
     {
         name: 'Apr',
         Usage: 3780,
-        Goal: 4908,
     },
     {
         name: 'May',
         Usage: 4890,
-        Goal: 5950,
     },
     {
         name: 'Jun',
         Usage: 3390,
-        Goal: 3800,
     },
     {
         name: 'Jul',
         Usage: 4490,
-        Goal: 5300,
     },
     {
         name: 'Aug',
         Usage: 2220,
-        Goal: 4100,
     },
     {
         name: 'Sep',
         Usage: 4500,
-        Goal: 5000,
     },
     {
         name: 'Oct',
         Usage: 3300,
-        Goal: 4298,
     },
     {
         name: 'Nov',
         Usage: 5200,
-        Goal: 6800,
     },
     {
         name: 'Dec',
         Usage: 3780,
-        Goal: 4908,
     },
 ];
 
@@ -84,7 +72,7 @@ class YearlyChart extends PureComponent {
                     <YAxis tick={{ fill: '#1e293b' }} />
                     <Tooltip />
                     <Area type="monotone" dataKey="Usage" stackId="1" stroke="#a8a29e" fill="#a8a29e" />
-                    <Area type="monotone" dataKey="Goal" stackId="1" stroke="#10B981" fill="#10B981" />
+                    <Area type="monotone" dataKey="" stackId="1" stroke="#10B981" fill="#10B981" />
                 </AreaChart>
             </ResponsiveContainer>
         );
