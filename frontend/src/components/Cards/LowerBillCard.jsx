49f9b928c5af4489d738c64daf9da6576f91246c
DialloJones333 <diallojones23@gmail.com>
2024-07-01 11:34:57 -0400

    feat: Update SmartHomeCard, LowerEnergyCard, and LowerBillCard buttons to direct the user to useful links

--- frontend/src/components/Cards/LowerBillCard.jsx
+++ frontend/src/components/Cards/LowerBillCard.jsx
@@ -8,7 +8,9 @@ const LowerBillCard = () => {
                 <h2 className="card-title">Want to Lower Your Energy Bill?</h2>
                 <p>Check out this page to find out how!</p>
                 <div className="card-actions justify-end">
-                    <button className="btn bg-stone-400 text-gray-100 hover:bg-emerald-500">Learn More</button>
+                    <a href='https://www.energy.gov/energysaver/energy-saver-guide-tips-saving-money-and-energy-home' target="_blank" rel="noopener noreferrer">
+                        <button className="btn bg-stone-400 text-gray-100 hover:bg-emerald-500">Learn More</button>
+                    </a>
                 </div>
             </div>
         </div>
